9009a9884662598fc3dc839f63e201a275dc3cc7
Jarek Kostrzewa <student@10.10.26.16>
2015-04-27 10:22:58 +0200

    Added implementation of BetterRadar with Executore

--- src/main/java/edu/iis/mto/multithread/BetterRadar.java
+++ src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -1,5 +1,36 @@
 package edu.iis.mto.multithread;
 
-public class BetterRadar {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executor;
 
+public class BetterRadar 
+{
+	
+	private PatriotBattery battery;
+	private Executor launcher;
+
+	public BetterRadar(PatriotBattery missle) {
+		this.battery = missle;
+	}
+
+	public void notice(Scud enemyMissle) {
+		launchPatriot();
+	}
+
+	private void launchPatriot() {
+		Runnable launchPatriotTask = new Runnable() {
+			public void run() {
+			for (int i = 0; i < 10; i++) 
+				battery.launchPatriot();
+				
+			}
+		};
+		
+		launcher.execute(launchPatriotTask);
+	}
+
+	public void setLauncher(Executor Executor) {
+		this.launcher = Executor;
+	}
+	
 }
